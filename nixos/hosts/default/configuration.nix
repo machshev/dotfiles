3873b0cb188ef2779715e8243eea5f4fa797223f
David James McCorrie <djmccorrie@gmail.com>
2024-07-03 21:56:20 +0100

    Add tailscale

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -197,6 +197,8 @@
     ];
   };
 
+  services.tailscale.enable = true;
+
   environment.sessionVariables = {
     POLKIT_AUTH_AGENT = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
     GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
